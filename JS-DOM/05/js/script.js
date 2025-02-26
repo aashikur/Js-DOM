dc7e55edca3bafc515ce4d5573c407cd35e26967
aashikur <mdaashikur@gmail.com>
2025-02-26 07:00:05 +0600

    classList

--- JS-DOM/05/js/script.js
+++ JS-DOM/05/js/script.js
@@ -7,6 +7,8 @@ document.querySelectorAll('')
 document.querySelectorAll('');
 innerHTML
 innerText
+
+
 */
 
 
@@ -20,3 +22,22 @@ let x = document.getElementById('title');
     x.style.borderRadius = '5px';
     x.style.maxWidth = '600px';
     x.style.margin = 'auto';
+
+// Query Selector
+
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+    section.classList.add('hover-effect');
+
+    section.style.border = '1px solid #ddd';
+    section.style.padding = '40px 20px';
+    section.style.margin = '10px 0';
+    section.style.borderRadius = '5px';
+    section.style.background = "yellowgreen"
+}
+
+// Selecting from Id and adding Class
+ const section1 = document.getElementById('s1');
+ section1.classList.add('text-color') 
+
+ 
