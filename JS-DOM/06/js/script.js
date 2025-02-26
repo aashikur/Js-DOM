8179394692e8dcf54abe9925b85e1f0ba8afb124
aashikur <mdaashikur@gmail.com>
2025-02-26 08:23:10 +0600

    append

---
+++ JS-DOM/06/js/script.js
@@ -0,0 +1,13 @@
+/*
+    DOM NODES Basic wHat is it and how work
+    FirstChild
+    Sibling
+    nextChild
+    CreateElement
+    innerText
+    append 
+    appendChild
+    (add new list item in exsisting list)
+    parentNode
+
+*/
