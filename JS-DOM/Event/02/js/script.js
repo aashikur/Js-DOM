701dc76ee73a4d30b7b476e5fab0250667222f6e
aashikur <mdaashikur@gmail.com>
2025-02-27 21:30:04 +0600

    user login funny DOM

---
+++ JS-DOM/Event/02/js/script.js
@@ -0,0 +1,8 @@
+        // catch the btn
+        const btn = document.getElementById('btn');
+
+        btn.addEventListener('click',function(){
+            // catch title
+           const title = document.getElementById('title-text');
+                 title.innerHTML = "You Have Loged In";
+        })
