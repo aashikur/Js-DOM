52f14fb02876e34d913c9d61cdbbaf13fd777b79
aashikur <mdaashikur@gmail.com>
2025-03-12 00:35:35 +0600

    '.'

---
+++ Js-E6-Advance/06/scripts/main.js
@@ -0,0 +1,16 @@
+What will be the output? 
+
+const cube=x=> x*x*x; 
+console.log(cube(2))
+A
+
+2
+B
+
+4
+C
+
+6
+D
+
+8
---
+++ Js-E6-Advance/07/scripts/recap.js
@@ -0,0 +1,23 @@
+// Primitive Data , Non-Primitive Data
+
+// let person = 'ashik';
+// let newP = person;
+
+// newP = 'ashraf';
+// newP[2] = 'T';
+// console.log(newP);
+
+
+//  let money = {bangladesh: 'taka'}
+//  let myCurency = money;
+
+//  money.bangladesh = 'Dollar';
+
+
+//  console.log(myCurency);
+
+
+
+// Null , Undefinied
+
+// true , False Value
